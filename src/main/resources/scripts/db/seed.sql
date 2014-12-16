62f863237901b8b90d360793a81df7cf04d5ae57
Breno Kusunoki <brenokusunoki@gmail.com>
2014-12-15 22:15:51 -0200

    #7 - Added SQL to create position entity and the names of the unprocessed responses wre refactored to Unprocessed Position

--- src/main/resources/scripts/db/seed.sql
+++ src/main/resources/scripts/db/seed.sql
@@ -1,1 +1,31 @@
-CREATE TABLE response_positions (id serial NOT NULL, bus_line integer, date_received timestamp with time zone, content character varying, CONSTRAINT pk_response_positions PRIMARY KEY (id)) WITH (OIDS=FALSE);
+CREATE TABLE unprocessed_position
+  ( 
+     id            SERIAL NOT NULL, 
+     bus_line      INTEGER, 
+     date_received TIMESTAMP WITH TIME zone, 
+     content       CHARACTER varying, 
+     CONSTRAINT pk_response_positions PRIMARY KEY (id) 
+  ) with (oids=FALSE); 
+
+CREATE TABLE position
+  ( 
+     id            SERIAL NOT NULL, 
+     bus_line      INTEGER, 
+     bus_direction CHARACTER varying, 
+     date_received TIMESTAMP WITH TIME zone, 
+     latitude      FLOAT, 
+     longitude     FLOAT,
+     type          INTEGER,
+     CONSTRAINT pk_position PRIMARY KEY (id)
+  ) with (oids=FALSE); 
+
+CREATE TABLE last_unprocessed_position_processed
+  ( 
+     id      SERIAL NOT NULL, 
+     last_id BIGINT, 
+     CONSTRAINT pk_last_unprocessed_position_processed PRIMARY KEY(id)
+  ) with (oids=FALSE); 
+
+INSERT INTO last_unprocessed_position_processed
+            (last_id) 
+VALUES      (0); 
